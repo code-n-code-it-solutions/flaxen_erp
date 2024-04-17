945ead14c03a07bfe36349a8adbfb2e759e7fb89
Syed Suhaib Zia <suhaibzia786@gmail.com>
2024-04-17 16:04:52 +0400

    updated

--- pages/erp/font-icons.tsx
+++ pages/erp/font-icons.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { setPageTitle } from '../@/store/slices/themeConfigSlice';
+import { setPageTitle } from '@/store/slices/themeConfigSlice';
 
 const FontIcons = () => {
     const dispatch = useDispatch();
