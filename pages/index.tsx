b687da53e1f5ab0ec4036f54b1cb5087db6ebd2d
Syed Suhaib Zia <suhaibzia786@gmail.com>
2024-07-12 14:36:01 +0400

    New version Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import WebsiteLayout from "@/components/Layouts/WebsiteLayout";
+import WebsiteLayout from '@/components/Layouts/WebsiteLayout';
+import Button from '@/components/Button';
+import { ButtonSize, ButtonType, ButtonVariant } from '@/utils/enums';
 
 const Index = () => {
     return (
-        <div>
-            Website
+        <div className="flex justify-center items-center h-screen">
+            <Button
+                type={ButtonType.link}
+                text="Go Workspace"
+                link={'/auth/signin'}
+                variant={ButtonVariant.primary}
+                size={ButtonSize.large}
+            />
         </div>
     );
 };
